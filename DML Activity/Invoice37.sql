42d5c26c96aa67dd1bfbc4d4dd0727a31e2acd14
EduardoCFong <95650723+EduardoCFong@users.noreply.github.com>
2021-12-15 15:59:30 -0500

    Update Invoice37.sql

--- DML Activity/Invoice37.sql
+++ DML Activity/Invoice37.sql
@@ -1,5 +1,6 @@
---How many line items were there for invoice #37?
-SELECT SUM(Quantity) FROM InvoiceLine WHERE InvoiceId = 37;
+--How many line items were there for invoice #37?
+
+SELECT COUNT(Quantity) FROM InvoiceLine WHERE InvoiceId = 37;
 
 
 
