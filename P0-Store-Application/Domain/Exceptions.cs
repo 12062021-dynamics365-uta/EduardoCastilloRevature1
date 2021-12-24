ebf85bd930c88418c08fd0193f8f12eaca332716
EduardoCFong <95650723+EduardoCFong@users.noreply.github.com>
2021-12-24 08:56:50 -0500

    Added exception class, work on Models project

---
+++ P0-Store-Application/Domain/Exceptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    internal class Exceptions
+    {
+
+    }
+
+    internal class OrderQuantityLimitException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "OrderQuantityLimitException: The maximum number of products per order is 50";
+            }
+        }
+    }
+
+    internal class OrderTotalCostLimitException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "OrderTotalCostLimitException: The maximum total cost per order is $500";
+            }
+        }
+    }
+}
+
