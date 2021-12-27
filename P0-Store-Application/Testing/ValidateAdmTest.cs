7c2f1d9abb3370ae82c5fcb71e7f8f0f6a53ff20
EduardoCFong <95650723+EduardoCFong@users.noreply.github.com>
2021-12-27 08:46:49 -0500

    P0 Project Testing added

---
+++ P0-Store-Application/Testing/ValidateAdmTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Xunit;
+using Models;
+
+namespace Testing
+{
+    public class ValidateAdmTest
+    {
+        [Fact]
+        public void ValidateAdm_StoreLogic_Test()
+        {
+            // Arrange
+            StoreLogic storeLogic = new StoreLogic();
+
+
+            // Act
+            bool result = storeLogic.ValidateAdm("123");
+
+
+
+            // Assert
+            Assert.True(result);           
+        }
+    }
+}
