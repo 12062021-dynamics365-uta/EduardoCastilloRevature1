f2c7f50a45ca69a16383f543b6d46da8a0c3719e
EduardoCFong <95650723+EduardoCFong@users.noreply.github.com>
2022-01-05 12:08:16 -0500

    Sweet’nSalty JS done

--- CodeAssignment-Eduardo-Castillo/SweetnSaltyJs/javascript.js
+++ CodeAssignment-Eduardo-Castillo/SweetnSaltyJs/javascript.js
@@ -1,35 +1,42 @@
 let count = 1;
-let sweetnSalty = 0;
-let salty = 0;
-let sweet = 0;
+let sweetnSalty=0;
+let salty=0;
+let sweet=0;
 
+let consoleLine = "";
 
 
 while (count <= 1000) {
     //The while loop is for counting to 1000, the for loop is for printing in groups of 20
     for (let i = 0; i < 20; i++) {
+
+
         // if/else decide between three cases, divisible by 5, divisible by 3 or by none
         if (count % 5 == 0) {
             if (count % 3 == 0)//Check if it is also divisible by 3
             {
-                console.log("sweet’nSalty ");
+                consoleLine += "sweet’nSalty";
                 sweetnSalty++;
             }
             else {
-                console.log("salty ");
+                consoleLine += "salty ";
                 salty++;
             }
         }
         else if (count % 3 == 0) {
-            console.log("sweet ");
+            consoleLine += "sweet ";
             sweet++;
         }
         else {
-            console.log(count + " ");
-        }
+            consoleLine += count + " ";
+        }       
         count++;
+
     }//End for loop
 
+
+    console.log(consoleLine);//Print a line to the console
+    consoleLine = ""; //clear for the next time loop
 
 console.log(' ');   
 }//End while loop
@@ -37,5 +44,5 @@ console.log(' ');
 
 
 console.log();
-console.log(" ------------------------------------------------------------------------");
+console.log("------------------------------------------------------------------------");
 console.log(`Sweets: ${sweet}      Salties: ${salty}       Sweet’nSalties: ${sweetnSalty}`);
